78ccc5230eb061d837840668c208180b6a47c873
Vinothm22 <mvino549@gmail.com>
2021-09-07 21:54:03 +0530

    Username Functionality

--- src/org/log/Log.java
+++ src/org/log/Log.java
@@ -1,5 +1,14 @@
 package org.log;
 
 public class Log {
+	
+	public static void userName() {
+		System.out.println("Username");
+	}
+	
+	public static void main(String[] args) {
+		
+		userName();
+	}
 
 }
