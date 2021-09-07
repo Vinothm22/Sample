53485c724b90a7dd05a3f8bcb9a0f6d49e6437ed
Vinothm22 <mvino549@gmail.com>
2021-09-08 00:03:54 +0530

    Password Functionality

--- src/org/log/Log.java
+++ src/org/log/Log.java
@@ -1,5 +1,14 @@
 package org.log;
 
 public class Log {
+	
+	public static void passWord() {
+		System.out.println("Password");
+	}
 
-}
+	public static void main(String[] args) {
+		
+		passWord();
+	}
+
+ }
